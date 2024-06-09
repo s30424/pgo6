b4c37915ec9e90ff5385f4fc22a1496a8232e9b7
s30424 <162446187+s30424@users.noreply.github.com>
2024-06-09 13:06:51 +0200

    Update Main.java

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -1,14 +1,13 @@
 import java.util.Date;
-public class Main {
-    public static int counter = 0;
+import java.util.ArrayList;
 
+public class Main {
     public static void main(String[] args) {
-        Student s = new Student("John", "Doe", "doe@wp.pl", "Warsaw, Zlota 12",
-                "333-322-222", new Date(1980, 1, 1));
-        StudyProgramme it = new StudyProgramme("IT", "AAA", 7, 5);
-        s.enrollStudent(i                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            t);
-        s.addGrade(5, "PGO");
-        s.addGrade(2, "APBD");
+        Student s=new Student("Donald", "Vygi", "doe@wp.pl", "Warsaw, Zlota 44", "333-322-222", new Date(1980, 1, 1));
+                StudyProgramme it=new StudyProgramme("IT", "AAA", 7, 5);
+        s.enrollStudent(it); 
+        s.addGrade(5, "MAT");
+        s.addGrade(4, "PPZ");
         Students.promoteAllStudents();
         Students.displayInfoAboutAllStudents();
     }
